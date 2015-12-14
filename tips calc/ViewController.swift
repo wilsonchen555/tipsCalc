37717bef3aba9d39b5b85ec6e145f6b2e2ce9a1a
Wilson Chen <Wilson@wilsons-air.mobile.asu.edu>
2015-12-14 13:54:31 -0700

    Initial Commit

---
+++ tips calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tips calc
+//
+//  Created by Wilson Chen on 12/14/15.
+//  Copyright © 2015 Wilson Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
