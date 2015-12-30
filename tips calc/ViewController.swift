a84171732819d2e8264ee944cbe011a42606c9ee
Wilson Chen <Wilson@Wilsons-MacBook-Air.local>
2015-12-29 22:36:55 -0700

    added .gitignore .gitattributes

--- tips calc/ViewController.swift
+++ tips calc/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        var tipPercentages = [0.05, 0.15, 0.20]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        //print("User editing bill") just to see if it's working
+        var billAmount = NSString(string: billField.text!).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
 
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
